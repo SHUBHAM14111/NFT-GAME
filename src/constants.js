062d36f0de255fc90a9bb7e2c4cccd69ddc2bd57
Shubham Gandhi <paskgandhi@gmail.com>
2021-10-24 22:58:08 +0530

    Deploy on vercel

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0xf7ebA37298907aF26D155233Fd3c9c7A36282954";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
